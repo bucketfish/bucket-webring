0ed97036fa7809782578a50d0a3046dfeeb0635c
bucketfishy <bucketfishy@gmail.com>
2024-11-11 15:48:45 +0800

    convert html special chars in name

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ const DATA_FOR_WEBRING = `https://raw.githubusercontent.com/bucketfishy/bucket-w
 const list = document.getElementById("peoplelist");
 const count = document.getElementById("peoplecount");
 
+function convertHTML(str) {
+  var regexTable =  {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    '\'': '&apos;'
+    };
+
+  let result = str;
+
+  var regexKeys = Object.keys(regexTable);
+
+  for (var i=0; i<regexKeys.length; i++) {
+
+    let regex = new RegExp(regexKeys[i], 'g');
+    result = result.replace(regex, regexTable[regexKeys[i]]);
+  }
+
+  return result;
+}
 
 fetch(DATA_FOR_WEBRING)
   .then((response) => response.json())
@@ -11,7 +32,7 @@ fetch(DATA_FOR_WEBRING)
     count.innerHTML = sites.length;
 
     for (var i = 0; i < sites.length; i++){
-      list.innerHTML += "<div><p><a href=" + sites[i].url + ">" + sites[i].name + "</a></p></div>";
+      list.innerHTML += "<div><p><a href=" + sites[i].url + ">" + convertHTML(sites[i].name) + "</a></p></div>";
     }
 
 
